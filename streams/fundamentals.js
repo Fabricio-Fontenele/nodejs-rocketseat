ef71ad6a1bd86946f22eb75a1b15541b1ad84ef6
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-01-12 12:32:02 -0300

    add stream fundamentals

---
+++ streams/fundamentals.js
@@ -0,0 +1,40 @@
+// streams ->
+
+import { Readable, Writable, Transform } from "node:stream";
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
+
+        this.push(buf);
+      }
+    }, 1000);
+  }
+}
+
+class TransformToNegative extends Transform {
+  _transform(chunk, encoding, callback) {
+    const negatedNumber = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(negatedNumber)));
+  }
+}
+
+class MultipleByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new TransformToNegative())
+  .pipe(new MultipleByTenStream());
